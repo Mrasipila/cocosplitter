f0d2cb341d405211de5612ab0c85d3ff9b663ae0
Meyssan KANZARI <meyssank@outlook.fr>
2023-07-06 17:52:02 +0200

    coco

---
+++ README.md
@@ -0,0 +1,43 @@
+Simple tool to split coco annotations file and images file into the two sets of training and validation. No external module is neaded, only python.
+The data is shuffled before splitting, it creates valid annotation file with 'images', 'categories', 'annotations' and 'info' keys if it exist.
+
+# How it works
+
+```
+> python cocosplitter.py -h
+usage: cocosplitter.py [-h] --annotation-file ANNOT --destination-dirtrain DESTTRAIN --destination-dirtest DESTTEST
+                       --split SPLIT
+
+Split Coco annotation file and image file into training and test set
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --annotation-file ANNOT
+                        Name of the file containing Coco annotations
+  --destination-dirtrain DESTTRAIN
+                        Name of the directory that will contain the train set images
+  --destination-dirtest DESTTEST
+                        Name of the directory that will contain the test set images
+  --split SPLIT         Percentage of data that will be put in the train set, a float between [0;1]
+```
+
+both annotation file will be placed under the directory name `annotations`, file will be named `train.json` and  `test.json`, it is your wish to rename them or relocate them later on. 
+
+# Example running 
+
+``` python cocosplitter.py --annotation-file result.json --destination-dirtrain traindir --destination-dirtest testdir --split 0.9 ```
+
+# Data
+
+The file organisation will be as follows :
+
+```
+data/
+  ├── traindir/
+  ├── testdir/
+  └── annotations/
+  	├── train.json
+  	└── test.json
+```
+
+Useful in the task of object detection when you want to follow the file organisation of https://github.com/Sense-X/Co-DETR/blob/main/README.md
---
+++ cocosplitter.py
@@ -0,0 +1,63 @@
+import json
+import random
+import argparse
+import os
+import shutil
+
+parser = argparse.ArgumentParser(description='Split Coco annotation file and image file into training and test set') 
+parser.add_argument('--annotation-file',type=str,required=True,dest="annot", help="Name of the file containing Coco annotations")
+parser.add_argument('--destination-dirtrain',type=str,required=True,dest="desttrain", help="Name of the directory that will contain the train set images")
+parser.add_argument('--destination-dirtest',type=str,required=True,dest="desttest", help="Name of the directory that will contain the test set images")
+parser.add_argument('--split',type=float,required=True,dest="split", help="Percentage of data that will be put in the train set, a float between [0;1]")
+
+args = parser.parse_args()
+
+def main(args):
+    
+    with open(args.annot,'r') as f:
+        coco = json.load(f)
+        try:
+            categories = coco['categories']
+            images = coco['images']
+            annotations = coco['annotations']
+            infos = coco['info']
+        except:
+            pass
+        
+    nb_images = len(images)
+    image_index = list(range(nb_images))
+    random.shuffle(image_index)
+    train_indexes = image_index[:int(nb_images*args.split)]
+    test_indexes = image_index[int(nb_images*args.split):]
+    
+    # for annotations files
+    train_images = [images[i] for i in train_indexes]
+    test_images = [images[i] for i in test_indexes]
+
+    train_annotations = [annot for i in train_indexes for annot in annotations if annot['image_id'] == i]
+    test_annotations = [annot for i in test_indexes for annot in annotations if annot['image_id'] == i]
+    
+    if infos : 
+        result_train = {'images':train_images, 'categories':categories, 'annnotations':train_annotations, 'info':infos}
+        result_test = {'images':test_images, 'categories':categories, 'annnotations':test_annotations, 'info':infos}
+    else :
+        result_train = {'images':train_images, 'categories':categories, 'annnotations':train_annotations}
+        result_test = {'images':test_images, 'categories':categories, 'annnotations':test_annotations}
+    
+    os.mkdir('annotations')
+    with open('annotations/train.json','w') as f:
+        json.dump(result_test,f,indent=4)
+    
+    with open('annotations/test.json','w') as f:
+        json.dump(result_test,f,indent=4)
+    
+    # for images files
+    os.mkdir(args.desttrain)
+    [shutil.copy(f'{filename}',args.desttrain) for filename in [tr['file_name'] for tr in train_images]]
+
+    os.mkdir(args.desttest)
+    [shutil.copy(f'{filename}',args.desttest) for filename in [tr['file_name'] for tr in test_images]]
+
+if __name__=="__main__":
+    main(args)
+    
